60b95e858d68361660cf5004b94e6414387f09c1
Rubeun Tan <github@rubeun.com>
2024-11-25 02:03:53 -0800

    add handleGuessEntry, window event listener for keydown

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { WORDS } from './data/words';
 import WordGrid from './components/WordGrid';
 
 const App = () => {
-  const [wordOfTheDay, setWordOfTheDay] = useState("brief"); // ### TODO - pull this from WORDS by date
-  const [wordOfTheDayArr, setWordOfTheDayArr] = useState(wordOfTheDay.split(""));
+  const [wordOfTheDay, setWordOfTheDay] = useState<string>("brief"); // ### TODO - pull this from WORDS by date
+  const [wordOfTheDayArr, setWordOfTheDayArr] = useState<string[]>(wordOfTheDay.split(""));
+
+  const [wordFound, setWordFound] = useState<boolean>(false);
+  const [currentGuessArr, setCurrentGuessArr] = useState<string[]>([]);
 
   // Checks if valid 5-letter word (in WORDS)
   const isValidWord = (guessWordArr: string[]) => {
@@ -27,6 +30,26 @@ const App = () => {
       return false;
     }
   };
+
+  const handleGuessEntry = (event: KeyboardEvent) => {
+    const keyPressed: string = event.key;
+    let guessLetters: string[] = [...currentGuessArr];
+    console.log("Key pressed: ", keyPressed);
+    
+    if (guessLetters.length < 5) {
+      guessLetters.push(keyPressed);
+      setCurrentGuessArr(guessLetters);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleGuessEntry);
+
+    return () => {
+      window.removeEventListener("keydown", handleGuessEntry);
+    };
+  }, [handleGuessEntry]);
+
 
   return (
     <div className="container">
