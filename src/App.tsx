4cddb00e712d6895e3b25ab91b12eea91b234cea
Rubeun Tan <github@rubeun.com>
2024-11-25 01:41:26 -0800

    add letterInWord, isWordOfTheDay

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,26 @@ const App = () => {
   const [wordOfTheDay, setWordOfTheDay] = useState("brief"); // ### TODO - pull this from WORDS by date
   const [wordOfTheDayArr, setWordOfTheDayArr] = useState(wordOfTheDay.split(""));
 
+  // Checks if valid 5-letter word (in WORDS)
   const isValidWord = (guessWordArr: string[]) => {
     const guessWord = guessWordArr.join("").toLowerCase();
     return WORDS.includes(guessWord);
   }
+
+  // Checks the letter to see if its in the word.
+  const letterInWord = (guessLetter: string) => {
+    const wordOfTheDayArr = wordOfTheDay.split("");
+    return wordOfTheDayArr.includes(guessLetter.toLowerCase());
+  };
+
+  // Checks if is wordOfTheDay
+  const isWordOfTheDay = (guessWordArr: string[]) => {
+    if (guessWordArr.join("") == wordOfTheDay) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   return (
     <div className="container">
