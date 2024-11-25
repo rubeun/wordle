bab4bed3ebebc1d4ab2ce94f52189a191dc36c15
Rubeun Tan <github@rubeun.com>
2024-11-24 17:51:05 -0800

    add WordRowEmpty component and styles

--- src/components/WordGrid.tsx
+++ src/components/WordGrid.tsx
@@ -1,9 +1,28 @@
 import styles from './WordGrid.module.css';
 
+const WordRowEmpty = () => {
+  const emptyLetters = ["", "", "", "", ""];
+
+  return (
+    <div className={styles.wordRow}>
+      {emptyLetters.map((letter: string, index: number) => {
+        return (
+          <div
+            key={`word-row-empty-${index}`}
+            className={styles.wordRowLetter}
+          >
+            {letter}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 const WordGrid = () => {
   return (
     <div className={styles.wordGrid}>
-      <p>WordGrid</p>
+      <WordRowEmpty />
     </div>
   );
 };
