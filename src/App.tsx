20ae8dd19d60c404937c68af6b93b1046d2f67dd
Rubeun Tan <github@rubeun.com>
2024-11-24 17:11:55 -0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>Rubeun's Wordle App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
