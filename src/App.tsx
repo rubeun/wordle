67c9b89a39feff3926a6adf448a839e48134b96f
Rubeun Tan <github@rubeun.com>
2024-11-25 01:34:50 -0800

    add isValidWord fn, wordOfTheDay hardcoded

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { WORDS } from './data/words';
 import WordGrid from './components/WordGrid';
 
 const App = () => {
+  const [wordOfTheDay, setWordOfTheDay] = useState("brief"); // ### TODO - pull this from WORDS by date
+  const [wordOfTheDayArr, setWordOfTheDayArr] = useState(wordOfTheDay.split(""));
+
+  const isValidWord = (guessWordArr: string[]) => {
+    const guessWord = guessWordArr.join("").toLowerCase();
+    return WORDS.includes(guessWord);
+  }
+
   return (
     <div className="container">
       <header className="header">
