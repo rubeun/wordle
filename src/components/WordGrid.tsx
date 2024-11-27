c0b8bce782d10a02b46735e79e03b0fbe4ce4f2b
Rubeun Tan <github@rubeun.com>
2024-11-27 03:57:13 -0800

    add WordRow component, populate rest with WordRowEmpty

--- src/components/WordGrid.tsx
+++ src/components/WordGrid.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { cursorTo } from 'readline';
 import styles from './WordGrid.module.css';
 
@@ -5,9 +6,16 @@ type WordRowActive = {
   currentGuessArr: string[];
 };
 
+type WordRow = {
+  guessWordArr: string[];
+  answerWordArr: string[];
+};
+
 type WordGrid = {
-  currentGuessArr: string[],
-}
+  allGuessesArr: string[][];
+  answerWordArr: string[];
+  currentGuessArr: string[];
+};
 
 const WordRowEmpty = () => {
   const emptyLetters = ["", "", "", "", ""];
@@ -53,13 +61,66 @@ const WordRowActive = ({ currentGuessArr }: WordRowActive) => {
   );
 };
 
-const WordGrid = ({ currentGuessArr }: WordGrid) => {
+const WordRow = ({ guessWordArr, answerWordArr }: WordRow) => {
   return (
-    <div className={styles.wordGrid}>
-      <WordRowActive currentGuessArr={currentGuessArr} />
-      <WordRowEmpty />
+    <div className={styles.wordRow}>
+      {guessWordArr.map((guessLetter: string, index: number) => {
+        const inAnswerWord = answerWordArr.includes(guessLetter);
+        const isRightPlace = guessLetter === answerWordArr[index];
+        return (
+          <div
+            key={`word-row-${index}`}
+            className={`${styles.wordRowLetter} ${
+              isRightPlace
+                ? styles.rightLetterRightPlace
+                : inAnswerWord
+                ? styles.rightLetterWrongPlace
+                : ""
+            }`}
+          >
+            {guessLetter}
+          </div>
+        );
+      })}
     </div>
   );
 };
+
+const WordGrid = ({
+  allGuessesArr,
+  answerWordArr,
+  currentGuessArr,
+  }: WordGrid) => {
+    const [filledGuessesArr, setFilledGuessesArr] = useState<string[][]>([]);
+    const [emptyArrays, setEmptyArrays] = useState<string[]>([]);
+    useEffect(() => {
+      let tempEmpty = [];
+      if (allGuessesArr.length < 6) {
+        tempEmpty = Array(5 - allGuessesArr.length).fill(["", "", "", "", ""]);
+      }
+      setEmptyArrays([...tempEmpty]);
+      setFilledGuessesArr([...allGuessesArr]);
+    }, [allGuessesArr]);
+
+    return (
+      <div className={styles.wordGrid}>
+        {filledGuessesArr.length > 0
+          ? filledGuessesArr.map((wordArr: string[], index: number) => (
+              <WordRow
+                key={index}
+                guessWordArr={wordArr}
+                answerWordArr={answerWordArr}
+              />
+            ))
+          : null}
+        {filledGuessesArr.length < 6 ? (
+          <WordRowActive currentGuessArr={currentGuessArr} />
+        ) : null}
+  
+        {emptyArrays.length > 0
+          ? emptyArrays.map((empty, index) => <WordRowEmpty key={index} />)
+          : null}
+      </div>
+    );};
 
 export default WordGrid;
