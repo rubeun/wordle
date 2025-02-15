db32d0c0cb21f356e5645d0b17243036f7d57696
Rubeun Tan <github@rubeun.com>
2025-02-15 15:32:59 -0800

    custom hooks to manage score

---
+++ src/hooks/useUserInfo.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export const useUserInfo = () => {
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    wins: 0,
+    losses: 0,
+    guessIn1: 0,
+    guessIn2: 0,
+    guessIn3: 0,
+    guessIn4: 0,
+    guessIn5: 0,
+    guessIn6: 0,
+    previousWordOfTheDays: [],
+  })
+
+  const addWin = () => {
+    setUserInfo(userInfo => ({
+      ...userInfo,
+      wins: userInfo.wins++,
+    }))
+  }
+
+  const addLoss = () => {
+    setUserInfo(userInfo => ({
+      ...userInfo,
+      losses: userInfo.losses++,
+    }))
+  }
+
+  const addPreviousWordOfTheDay = ( word ) => {
+    setUserInfo(userInfo => ({
+      ...userInfo,
+      previousWordOfTheDays: [...userInfo.previousWordOfTheDays, word],
+    }))
+  }
+  
+
+  return {
+    userInfo,
+    addWin,
+    addLoss,
+    addPreviousWordOfTheDay,
+  }
+}
