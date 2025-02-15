213335585456165c35bc26704e807c967d603a59
Rubeun Tan <github@rubeun.com>
2025-02-15 15:31:15 -0800

    pass userInfo into Popup for score

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,21 +1,51 @@
 import styles from './Popup.module.css';
 
-type PopupType = {
-  wordStatus: string;
+type UserInfoType = {
+  username: string,
+  wins: number,
+  losses: number,
+  guessIn1: number,
+  guessIn2: number,
+  guessIn3: number,
+  guessIn4: number,
+  guessIn5: number,
+  guessIn6: number,
+  previousWordOfTheDays: string[],
 }
 
-const Popup = ({ wordStatus }: PopupType) => {
+type PopupType = {
+  wordStatus: string;
+  wordOfTheDay: string;
+  userInfo: UserInfoType;
+  resetBoard: any;
+}
+
+const Popup = ({ wordStatus, wordOfTheDay, userInfo, resetBoard }: PopupType) => {
+  const wins = userInfo.wins;
+  const losses =  (userInfo.previousWordOfTheDays.length) - userInfo.wins;
   return (
     <div className={styles.popupContainer}>
       {wordStatus === "correct" 
-        ? (<div><h3>Correct Word Guessed!</h3></div>)
+        ? ( <div>
+              <h3>Correct Word <span className={styles.greenWord}>{wordOfTheDay}</span> Guessed!</h3>
+              <h5>Wins: {wins}</h5>
+              <h5>Losses: {losses}</h5>
+              <h5><button onClick={resetBoard}>Load Next Word</button></h5>
+            </div>)
         : wordStatus === "duplicate"
           ? (<div>Already guessed</div>)
           : wordStatus === "invalid"
             ? (<div>Invalid Word!</div>)
             : wordStatus === "short"
               ? (<div>Word too short!</div>)
-              : null
+              : wordStatus === "lost"
+                ? ( <div>
+                  <h3>The Correct Word was <span className={styles.greenWord}>{wordOfTheDay}</span></h3>
+                  <h5>Wins: {wins}</h5>
+                  <h5>Losses: {losses}</h5>
+                  <h5><button onClick={resetBoard}>Load Next Word</button></h5>
+                </div>)
+                : null  
       }
     </div>
   );
