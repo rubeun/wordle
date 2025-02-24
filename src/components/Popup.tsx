f8751b038b3563876732399881b0ef0f480c339f
Rubeun Tan <github@rubeun.com>
2025-02-23 21:06:44 -0800

    add GuessDistribution and styles

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -20,14 +20,45 @@ type PopupType = {
   resetBoard: any;
 }
 
+const GuessDistribution = ({ userInfo }: any) => {
+  const numberOfGuesses = userInfo.previousWordOfTheDays.length;
+  let percentagesArr = [];
+
+  for (let i = 1; i < 7; i++) {
+    const percentage = ((userInfo[`guessIn${i}`] / numberOfGuesses) * 100)
+    percentagesArr.push({
+      width: `${percentage}%`
+    })
+  }
+
+  return (
+    <div className={styles.guessInfo}>
+      {percentagesArr.map((style, index) => (
+        <div className={styles.guessBar}>
+          <span>{index + 1}: </span>
+          <div className={styles.guessPercentage} style={style}>
+            {
+              userInfo[`guessIn${index + 1}`] > 0 
+                ? userInfo[`guessIn${index + 1}`] 
+                : ''
+            }
+          </div>
+        </div>))}
+    </div>
+  );
+
+}
+
 const Popup = ({ wordStatus, wordOfTheDay, userInfo, resetBoard }: PopupType) => {
   return (
     <div className={styles.popupContainer}>
       {wordStatus === "correct" 
         ? ( <div>
-              <h3 className={styles.glowGreen}>Correct Word Guessed!</h3>
+              <h3>Correct Word Guessed!</h3>
               <h5>Wins: {userInfo.wins}<br />
                 Losses: {userInfo.losses}<br />
+                Guess Distribution: <br />
+                <GuessDistribution userInfo={userInfo} />
                 <button className={styles.nextWord} onClick={resetBoard}>Load Next Word</button>
               </h5>
             </div>)
