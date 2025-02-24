814a3872294347d55f94b418eec10eef51cc1d3f
Rubeun Tan <github@rubeun.com>
2025-02-23 16:32:33 -0800

    add dark mode button to header with styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import styles from './Header.module.css';
+import { IconMoon, IconSunFilled } from '@tabler/icons-react';
 
-const Header = () => {
+type HeaderType = {
+  isDarkMode: boolean,
+  toggleDarkMode: any,
+}
+
+const Header = ({ isDarkMode, toggleDarkMode}: HeaderType) => {
   return ( 
-    <header className={`${styles.header} ${styles.glowHeader}`}>
-      <h1>Rubeun's Wordle</h1>
+    <header className={`${styles.header} `}>
+      <h1 className={styles.glowHeader}>Rubeun's Wordle</h1>
+      <div className={styles.darkModeIcon}>
+        {isDarkMode ? <IconSunFilled onClick={toggleDarkMode} /> : <IconMoon onClick={toggleDarkMode} />}
+      </div>
     </header>
 );
 }
